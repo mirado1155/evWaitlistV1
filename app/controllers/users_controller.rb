39f74743de00bd93537fb0c3e2950c60c1e23fff
mirado1155 <rjadams1991@gmail.com>
2025-01-21 13:31:24 -0800

    convert to API only application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    def show
-        return 'hello'
+    def index
+        @users = User.all
+        render json: @users
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module WaitlistAppV1
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.api_only
   end
 end
