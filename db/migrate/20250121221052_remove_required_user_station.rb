05fd72d6ab5dcf1af105d9d3a3a360f6633889ac
mirado1155 <rjadams1991@gmail.com>
2025-01-21 14:37:21 -0800

    db migrate to remove null constraint on stations and users

---
+++ db/migrate/20250121221052_remove_required_user_station.rb
@@ -0,0 +1,5 @@
+class RemoveRequiredUserStation < ActiveRecord::Migration[8.0]
+  def change
+    change_column_null :users, :station_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_21_191924) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_21_221052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_21_191924) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.bigint "station_id", null: false
+    t.bigint "station_id"
     t.integer "waitlist_level_preference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
