c9c5dbbf59eae7baded12566a016ed12d29d40d6
mirado1155 <rjadams1991@gmail.com>
2025-01-21 15:14:30 -0800

    db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+@mock_users = ["Kurt", "Layne", "Eddie", "Chris", "Scott"]
+@mock_stations = ["La Honda", "Mountain View", "Arcata", "Madison", "San Mateo"]
+@mock_vehicles = [
+{user_id: 1, make: "Nissan", model: "Leaf", year: "2013"},
+{user_id: 2, make: "Honda", model: "Civic", year: "2001"},
+{user_id: 3, make: "Toyota", model: "Avalon", year: "2000"},
+{user_id: 3, make: "Subaru", model: "Forester", year: "2000"},
+{user_id: 4, make: "Kia", model: "EV6", year: "2022"},
+{user_id: 5, make: "Chevrolet", model: "Bolt", year: "2021"},
+{user_id: 5, make: "Toyota", model: "Tacoma", year: "2002"},
+]
+
+
+@mock_stations.each do |station_name|
+    Station.find_or_create_by!(name: station_name)
+end
+
+@mock_users.each do |user_name|
+    User.find_or_create_by!(username: user_name)
+end
+
+@mock_vehicles.each do |vehicle|
+    Vehicle.find_or_create_by!(
+    user_id: vehicle[:user_id], 
+    make: vehicle[:make], 
+    model: vehicle[:model], 
+    year: vehicle[:year]
+  )
+end
