d2af830276048f9e49d5415836ad0816fed73d8b
mirado1155 <rjadams1991@gmail.com>
2025-01-22 13:58:23 -0800

    migrate lat and long to station data and update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_225105) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_22_212004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_225105) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude", null: false
+    t.decimal "longitude", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,33 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-@mock_users = ["Kurt", "Layne", "Eddie", "Chris", "Scott"]
-@mock_stations = ["La Honda", "Mountain View", "Arcata", "Madison", "San Mateo"]
-@mock_vehicles = [
-{user_id: 1, make: "Nissan", model: "Leaf", year: "2013"},
-{user_id: 2, make: "Honda", model: "Civic", year: "2001"},
-{user_id: 3, make: "Toyota", model: "Avalon", year: "2000"},
-{user_id: 3, make: "Subaru", model: "Forester", year: "2000"},
-{user_id: 4, make: "Kia", model: "EV6", year: "2022"},
-{user_id: 5, make: "Chevrolet", model: "Bolt", year: "2021"},
-{user_id: 5, make: "Toyota", model: "Tacoma", year: "2002"},
+@mock_users = [ "Kurt", "Layne", "Eddie", "Chris", "Scott" ]
+
+@mock_stations = [
+    { name: "La Honda Pool", latitude: 37.318916, longitude: 122.269989 },
+    { name: "Mountain View Dana", latitude: 37.392329, longitude: 122.079095 },
+    { name: "Arcata Blondie's", latitude: 40.883292, longitude: 124.079932 },
+    { name: "Madison Capitol Square", latitude: 43.074663, longitude: 89.382122 },
+    { name: "San Mateo Station", latitude: 37.568057, longitude: 122.323809 }
 ]
 
+@mock_vehicles = [
+    { user_id: 1, make: "Nissan", model: "Leaf", year: "2013" },
+    { user_id: 2, make: "Honda", model: "Civic", year: "2001" },
+    { user_id: 3, make: "Toyota", model: "Avalon", year: "2000" },
+    { user_id: 3, make: "Subaru", model: "Forester", year: "2000" },
+    { user_id: 4, make: "Kia", model: "EV6", year: "2022" },
+    { user_id: 5, make: "Chevrolet", model: "Bolt", year: "2021" },
+    { user_id: 5, make: "Toyota", model: "Tacoma", year: "2002" }
+]
 
-@mock_stations.each do |station_name|
-    Station.find_or_create_by!(name: station_name)
+# TODO: Latitude and Longitude as decimals
+@mock_stations.each do |station|
+    Station.find_or_create_by!(
+        name: station[:name], 
+        latitude: station[:latitude], 
+        longitude: station[:longitude]
+    )
 end
 
 @mock_users.each do |user_name|
@@ -31,9 +43,9 @@ end
 
 @mock_vehicles.each do |vehicle|
     Vehicle.find_or_create_by!(
-    user_id: vehicle[:user_id], 
-    make: vehicle[:make], 
-    model: vehicle[:model], 
+    user_id: vehicle[:user_id],
+    make: vehicle[:make],
+    model: vehicle[:model],
     year: vehicle[:year]
   )
 end
